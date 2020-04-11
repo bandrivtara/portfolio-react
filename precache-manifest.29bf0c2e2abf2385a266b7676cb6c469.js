c69facb4d83258b31b170eba2bd534443133352f
Taras Bandrivskyy <bandrivtara@gmail.com>
2020-04-11 18:13:37 +0200

    Updates

---
+++ precache-manifest.29bf0c2e2abf2385a266b7676cb6c469.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec8f821098e1b3c8ad34a312d03a8cf",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "62f603a241197cfea6b2",
+    "url": "/portfolio-react/static/css/main.2a510eaf.chunk.css"
+  },
+  {
+    "revision": "a5cc271d3abcb5ed169b",
+    "url": "/portfolio-react/static/js/2.672255e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-react/static/js/2.672255e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62f603a241197cfea6b2",
+    "url": "/portfolio-react/static/js/main.488be72e.chunk.js"
+  },
+  {
+    "revision": "ded5e5aa532cde99b12e",
+    "url": "/portfolio-react/static/js/runtime-main.dfdf69bc.js"
+  }
+]);
