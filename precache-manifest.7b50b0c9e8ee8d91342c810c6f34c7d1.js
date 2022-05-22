200fa7d1ad6484ba66d210e211588ca659da0cb2
Bandrivskyy <tsbv@gft.com>
2022-05-22 17:26:26 +0200

    Updates

---
+++ precache-manifest.7b50b0c9e8ee8d91342c810c6f34c7d1.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5621f8b32f1a832a1720fb948439e8fb",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "92086b15fe21d4cd90ca",
+    "url": "/portfolio-react/static/css/main.f813b737.chunk.css"
+  },
+  {
+    "revision": "218d36d954f1b92495d3",
+    "url": "/portfolio-react/static/js/2.c66c4378.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio-react/static/js/2.c66c4378.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92086b15fe21d4cd90ca",
+    "url": "/portfolio-react/static/js/main.d413ce39.chunk.js"
+  },
+  {
+    "revision": "2f26d0aed242c262185b",
+    "url": "/portfolio-react/static/js/runtime-main.a4de92d3.js"
+  },
+  {
+    "revision": "9ce32096dc6e42d5a487f93b89f699c3",
+    "url": "/portfolio-react/static/media/DeskOfPlans.9ce32096.jpg"
+  },
+  {
+    "revision": "9cda8f585ebeb01afbafd5769284c19d",
+    "url": "/portfolio-react/static/media/LetsSurf.9cda8f58.png"
+  },
+  {
+    "revision": "bfc645850a268f6a353ac386efe26730",
+    "url": "/portfolio-react/static/media/SocialNetwork.bfc64585.png"
+  },
+  {
+    "revision": "6153a408fb897a5250deb375ba4f35eb",
+    "url": "/portfolio-react/static/media/aim.6153a408.svg"
+  },
+  {
+    "revision": "a4f4156732d7f75db6c82be7c256c398",
+    "url": "/portfolio-react/static/media/arrow-down.a4f41567.svg"
+  },
+  {
+    "revision": "56d2ccde2783dc585db74ff80634e0b1",
+    "url": "/portfolio-react/static/media/arrow-up.56d2ccde.svg"
+  },
+  {
+    "revision": "7b15aea9e0d320751fb9decf2de074f4",
+    "url": "/portfolio-react/static/media/creativity.7b15aea9.svg"
+  },
+  {
+    "revision": "49f25c5f6304f0fb428d57ba7c2a1847",
+    "url": "/portfolio-react/static/media/css.49f25c5f.svg"
+  },
+  {
+    "revision": "67cd34c6ab2d2e47e1bb7b86a0d97cae",
+    "url": "/portfolio-react/static/media/cursor.67cd34c6.svg"
+  },
+  {
+    "revision": "13de9b259df8b4baf8e1d66f0e4b2da4",
+    "url": "/portfolio-react/static/media/javascript.13de9b25.svg"
+  },
+  {
+    "revision": "92a6a388fdf24e78522311bc0af8f31b",
+    "url": "/portfolio-react/static/media/learn.92a6a388.svg"
+  },
+  {
+    "revision": "cc0c18f9f450c6b65c1be490b6cb29a9",
+    "url": "/portfolio-react/static/media/photo.cc0c18f9.jpg"
+  },
+  {
+    "revision": "780aad82bbeee38dd77902c3028a6595",
+    "url": "/portfolio-react/static/media/portfolio-react.780aad82.jpg"
+  },
+  {
+    "revision": "0be150011e89ac07951cf2ab7b643caf",
+    "url": "/portfolio-react/static/media/react.0be15001.svg"
+  },
+  {
+    "revision": "876640094098432b685b48360b66e83a",
+    "url": "/portfolio-react/static/media/redux.87664009.svg"
+  },
+  {
+    "revision": "9c75d613fba91e52b3a1db4bb14bcae3",
+    "url": "/portfolio-react/static/media/rest.9c75d613.svg"
+  },
+  {
+    "revision": "cffa0f7e5e3b86d0548331e6098767e5",
+    "url": "/portfolio-react/static/media/sass.cffa0f7e.svg"
+  },
+  {
+    "revision": "752c7d09febd08e44fe5b30473510e5e",
+    "url": "/portfolio-react/static/media/selfdevelop.752c7d09.svg"
+  },
+  {
+    "revision": "a548a0c7bf47eefdc5cb370a0a2ace8a",
+    "url": "/portfolio-react/static/media/teamplayer.a548a0c7.svg"
+  },
+  {
+    "revision": "2a0b68b44f19291b5ffbfda98c21b305",
+    "url": "/portfolio-react/static/media/time.2a0b68b4.svg"
+  },
+  {
+    "revision": "b978b605a10517a6a8e29783da6a5505",
+    "url": "/portfolio-react/static/media/typescript.b978b605.svg"
+  },
+  {
+    "revision": "6b80333ed8711fc74b48523466353ff5",
+    "url": "/portfolio-react/static/media/vuejs.6b80333e.svg"
+  }
+]);
